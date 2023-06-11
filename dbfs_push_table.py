df43e0408bd87e28589f9d4da6fd99b3fdadfce3
rdevcharles <rondell.charles@generalassemb.ly>
2023-06-10 22:26:27 -0400

    working on ssh functionality

--- dbfs_push_table.py
+++ dbfs_push_table.py
@@ -1,20 +1,19 @@
 import dbfs_parser
 import pysftp
 
-
-
-
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
 def push_table():
     args, parser = dbfs_parser.prog_parser()
-    
+
     sql_file = args.push
     if sql_file:
-        
-        print(sql_file)
-       
-        local_file_path = f'/path/to/{sql_file}'
-        remote_file_path = f'/path/on/server/{sql_file}'
 
-        with pysftp.Connection('server_address', username='your_username', password='your_password') as sftp:
-            sftp.put(local_file_path, remote_file_path)
-            sftp.close()
+        print(sql_file)
+
+        local_file_path = f'/config/{sql_file}.txt'
+        remote_file_path = ''
+
+        svr = pysftp.Connection('', username='', password='')
+        svr.put(local_file_path, remote_file_path)
+        svr.close()
