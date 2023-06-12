79a976ade67891590763d785f7b8c3534f049f00
rdevcharles <rondell.charles@generalassemb.ly>
2023-06-12 13:27:15 -0400

    fstp functionality working

--- dbfs_push_table.py
+++ dbfs_push_table.py
@@ -1,5 +1,6 @@
 import dbfs_parser
 import pysftp
+import os
 
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
@@ -9,11 +10,16 @@ def push_table():
     sql_file = args.push
     if sql_file:
 
-        print(sql_file)
+        try:
 
-        local_file_path = f'/config/{sql_file}.txt'
-        remote_file_path = ''
 
-        svr = pysftp.Connection('', username='', password='')
-        svr.put(local_file_path, remote_file_path)
-        svr.close()
+            local_file_path = f'./config/{sql_file}.txt'
+            remote_file_path = f"/home/dev/Desktop/projects/{sql_file}.txt"
+            svr = pysftp.Connection('', username='', password='')
+            #print(svr.listdir("/home/dev/Desktop/projects"))
+            svr.put(local_file_path,  remote_file_path)
+            svr.close()
+
+        except Exception as e:
+            print('An error occurred:', str(e))
+        
