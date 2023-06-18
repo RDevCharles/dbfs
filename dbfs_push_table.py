e9187de4ef519620f3b43e4f3cc9abac52743c7f
rdevcharles <rondell.charles@generalassemb.ly>
2023-06-17 23:10:40 -0400

    ec2 implementation

--- dbfs_push_table.py
+++ dbfs_push_table.py
@@ -1,9 +1,17 @@
+from dotenv import load_dotenv
+import os
 import dbfs_parser
 import pysftp
 
+load_dotenv()
+
 #sending over the file to the server
 cnopts = pysftp.CnOpts()
+
 cnopts.hostkeys = None
+cnopts.log = True
+
+
 def push_table():
     args, parser = dbfs_parser.prog_parser()
 
@@ -12,16 +20,22 @@ def push_table():
 
         try:
 
-
             local_file_path = f'./config/{sql_file}.txt'
             remote_file_path = f"/home/dev/Desktop/projects/{sql_file}.txt"
-            svr = pysftp.Connection('', username='', password='')
-            #print(svr.listdir("/home/dev/Desktop/projects"))
-            svr.put(local_file_path,  remote_file_path)
-            svr.close()
+            print("hello")
+            #host is the ip address of ec2 instance
+            #username is the user name of ec2 instance
+            #private_key is the path to the private key(pem file)
+            svr = pysftp.Connection(host=os.getenv("EC2HOST"), username=os.getenv(
+                "EC2USERNAME"), private_key=os.getenv("EC2PATH2PEM"), cnopts=cnopts)
+
+            print(svr.listdir("/home/ubuntu/"))
+            print("world")
+            #svr.put(local_file_path,  remote_file_path)
+            #svr.close()
 
         except Exception as e:
             print('An error occurred:', str(e))
-        
+
 #todo:
 #needs conditional to check that the file is an sql file
