34bc412d2576960d75b7c74d1d85d581032c8bbf
rdevcharles <rondell.charles@generalassemb.ly>
2023-06-19 11:56:37 -0400

    syntax changes

--- dbfs_push_table.py
+++ dbfs_push_table.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import dbfs_parser
+from dbfs_init import db_check
 import pysftp
 
 load_dotenv()
@@ -12,6 +13,9 @@ cnopts.hostkeys = None
 cnopts.log = True
 
 
+db_check()
+
+
 def push_table():
     args, parser = dbfs_parser.prog_parser()
 
@@ -20,22 +24,23 @@ def push_table():
 
         try:
 
-            local_file_path = f'./config/{sql_file}.txt'
-            remote_file_path = f"/home/dev/Desktop/projects/{sql_file}.txt"
+            local_file_path = f'./config/{sql_file}.sql'
+            remote_file_path = f"/home/{os.getenv('INSTANCEUSERNAME')}/{sql_file}.sql"
             print("hello")
             #host is the ip address of ec2 instance
             #username is the user name of ec2 instance
             #private_key is the path to the private key(pem file)
             svr = pysftp.Connection(host=os.getenv("EC2HOST"), username=os.getenv(
-                "EC2USERNAME"), private_key=os.getenv("EC2PATH2PEM"), cnopts=cnopts)
+                "INSTANCEUSERNAME"), private_key=os.getenv("EC2PATH2PEM"), cnopts=cnopts)
 
             print(svr.listdir("/home/ubuntu/"))
-            print("world")
-            #svr.put(local_file_path,  remote_file_path)
-            #svr.close()
+            print("dir found")
+            svr.put(local_file_path,  remote_file_path)
+            svr.close()
 
         except Exception as e:
             print('An error occurred:', str(e))
 
 #todo:
+#psql was installed on the ec2 instance
 #needs conditional to check that the file is an sql file
